251c306e00df4d554cfcccdbf2d888d9624c12c9
Pham Van Quan <quan100202@gmail.com>
2024-07-17 15:24:04 +0700

    update

--- Routes/ProductRouter.js
+++ Routes/ProductRouter.js
@@ -57,7 +57,7 @@ productRoute.get(
       res.json(product);
     } else {
       res.status(404);
-      throw new Error("Product not found!");
+      throw new Error("Details product not found!");
     }
   })
 );
